28a6fdb38a6bcb7280e3b329d6be2443012f1574
yoshiki namikawa <sinika-0923@hotmail.co.jp>
2018-04-18 15:49:08 +0900

    add animation

--- questionnaire/js/index.js
+++ questionnaire/js/index.js
@@ -1,17 +1,19 @@
 $(function () {
   let indexNo = 0;
   $('button').on('click', function () {
-    $(`div[data-index='${indexNo}']`).hide();
-    indexNo += 1;
-    $(`div[data-index='${indexNo}']`).show();
+    $(`div[data-index='${indexNo}']`).fadeOut("slow", function () {
+      indexNo += 1;
+      $(`div[data-index='${indexNo}']`).fadeIn("slow");
+    });
   });
 
   $('img').on('click', function () {
     let value = $(this).data("value");
     $(`div[data-index='${indexNo}'] input`).val(value);
 
-    $(`div[data-index='${indexNo}']`).hide();
-    indexNo += 1;
-    $(`div[data-index='${indexNo}']`).show();
+    $(`div[data-index='${indexNo}']`).fadeOut("slow", function () {
+      indexNo += 1;
+      $(`div[data-index='${indexNo}']`).fadeIn("slow");
+    });
   });
 })
