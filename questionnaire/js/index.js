94e774ea768d26ca68d1cd70ef962d33a7580f85
yoshiki namikawa <sinika-0923@hotmail.co.jp>
2018-04-18 16:34:45 +0900

    fix index.js

--- questionnaire/js/index.js
+++ questionnaire/js/index.js
@@ -1,6 +1,10 @@
 $(function () {
   let indexNo = 0;
   $('button').on('click', function () {
+    // 名前をクッキーに保存
+    let name = $("input[name='name']").val();
+    $.cookie("name", name);
+
     $(`div[data-index='${indexNo}']`).fadeOut("slow", function () {
       indexNo += 1;
       $(`div[data-index='${indexNo}']`).fadeIn("slow");
@@ -15,5 +19,24 @@ $(function () {
       indexNo += 1;
       $(`div[data-index='${indexNo}']`).fadeIn("slow");
     });
+  });
+
+  $("button#submit-button").on('click', function () {
+    let url = $(this).data('action');
+    let data = $('form').serializeArray();
+
+    $.ajax({
+      url: url,
+      type: 'post',
+      dataType: 'json',
+      data: JSON.stringify(data)
+    }).done((data) => {
+      console.log(data);
+      $('button#submit-button').attr('disabled', true);
+    }).fail((data) => {
+      console.log('error');
+    });
+
+    return false;
   });
 })
