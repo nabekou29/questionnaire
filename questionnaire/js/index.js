609567b70d7e60ca83208a9a47492e44a25802c7
yoshiki namikawa <sinika-0923@hotmail.co.jp>
2018-04-19 13:48:00 +0900

    fix js

--- questionnaire/js/index.js
+++ questionnaire/js/index.js
@@ -3,7 +3,7 @@ $(function () {
 
   $("input[name='name']").val($.cookie("name"));
 
-  $('button').on('click', function () {
+  $("button[type='button']").on('click', function () {
     // 名前をクッキーに保存
     let name = $("input[name='name']").val();
     $.cookie("name", name);
@@ -21,6 +21,10 @@ $(function () {
   });
 
   $("button#submit-button").on('click', function () {
+    $('button#submit-button').removeClass('btn-primary');
+    $('button#submit-button').addClass('btn-default');
+    $('button#submit-button').attr('readonly', true);
+
     let url = $(this).data('action');
     let data = $('form').serializeArray();
 
@@ -31,7 +35,6 @@ $(function () {
       data: JSON.stringify(data)
     }).done((data) => {
       console.log(data);
-      $('button#submit-button').attr('disabled', true);
     }).fail((data) => {
       console.log('error');
     });
