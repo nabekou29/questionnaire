49b71e3344ad5b9840642323aadc425cd8c82134
渡邉航平 <kohei@nabekounoMacBook.local>
2018-04-22 23:53:02 +0900

    fix: display graph

--- questionnaire/js/graph.js
+++ questionnaire/js/graph.js
@@ -1,82 +1,69 @@
-const DATASETS_FORMAT = {
-    q1 : {
-        label: "私生活",
-        borderColor: "rgba(249, 71, 66, 1)",
-        backgroundColor: "rgba(249, 71, 66, 0.2)",
-        spanGaps: true,
-        data: [],
-      },
-    q2 : {
-        label: "学校生活",
-        borderColor: "rgba(52, 151, 254, 1)",
-        backgroundColor: "rgba(52, 151, 254, 0.2)",
-        spanGaps: true,
-        data: [],
-      },
-    q3 : {
-        label: "人間関係",
-        borderColor: "rgba(245, 249, 66, 1)",
-        backgroundColor: "rgba(245, 249, 66, 0.2)",
-        spanGaps: true,
-        data: [],
-      },
-    q4 : {
-        label: "体調",
-        borderColor: "rgba(66, 148, 67, 1)",
-        backgroundColor: "rgba(66, 148, 67, 0.2)",
-        spanGaps: true,
-        data: [],
-      },
-    q5 : {
-        label: "プライズ",
-        borderColor: "rgba(249, 176, 66, 1)",
-        backgroundColor: "rgba(249, 176, 66, 0.2)",
-        spanGaps: true,
-        data: [],
-      },
-  };
-
 $(function() {
-  createGraph("渡邉", new Date(2018, 3, 20), new Date(2018, 3, 23));
+  // 名前一覧のセレクトボックス生成
+  let namesSelect = document.getElementById("names");
+  getAllNames().forEach(function(name) {
+    let option = document.createElement("option");
+    option.value = name;
+    option.innerText = name;
+    namesSelect.appendChild(option);
+  });
+
+
+  let dateFormat = new DateFormat("yyyy-MM-dd");
+  let setDate = new Date();
+  $("#to").val(dateFormat.format(setDate));
+  setDate.setDate(setDate.getDate() - 7);
+  $("#from").val(dateFormat.format(setDate));
+
+  // グラフ生成のイベント登録
+  $("#names, #to, #from").on('change', function() {
+    let name = $(this).val();
+    let from = dateFormat.parse($("#from").val());
+    let to = dateFormat.parse($("#to").val());
+    if (name) {
+      drawGraph(name, from, to);
+    }
+  });
 });
 
-function createGraph(name, from, to) {
-  var ctx = document.getElementById("lineChart").getContext('2d');
-  var lineChart = new Chart(ctx, {
-   type: 'line',
-   data: createGraphData(name, from, to),
-   options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                min: 1,
-                max: 5
-              }
-            }
-          ]
-        }
+/**
+ * グラフの描画
+ */
+function drawGraph(name, from, to) {
+  let ctx = document.getElementById("lineChart").getContext('2d');
+  ctx.clearRect(0, 0, ctx.width, ctx.height);
+  let lineChart = new Chart(ctx, {
+    type: 'line',
+    data: createGraphData(name, from, to),
+    options: {
+      scales: {
+        yAxes: [{ticks: {min: 1, max: 5}}]
       }
- });
+    }
+  });
 
-  $("#title").html(`${name}さんの${dateToString(from)} 〜 ${dateToString(to)}の心情`);
+  let dateFormat = new DateFormat("M/dd");
+  $("#graph-title").html(`${name}さんの${dateFormat.format(from)} 〜 ${dateFormat.format(to)}の心情変化`);
 }
 
+/**
+ * グラフ用のデータ作成
+ */
 function createGraphData(name, from, to) {
-  let answers = getAnswerByName(name);
+  let answers = getAnswersByName(name);
   let labels = [];
-  let dataSetsTmp = Object.assign({}, DATASETS_FORMAT);
+  let dataSetsTmp = getDatasetsTemplate();
 
   let dataIndex = 0;
   let targetDate = new Date(from.getTime());
   while (answers[dataIndex] < to) {
     dataIndex++;
   }
+  let dateFormat = new DateFormat("M/dd");
   for (; targetDate.getTime() <= to.getTime(); targetDate.setDate(targetDate.getDate() + 1)) {
-    labels.push(`${targetDate.getMonth() + 1}/${targetDate.getDate()}`);
-
+    labels.push(dateFormat.format(targetDate));
     let answer;
-    if (answers[dataIndex].date.getTime() == targetDate.getTime()) {
+    if (dataIndex < answers.length && answers[dataIndex].date.getTime() == targetDate.getTime()) {
       answer = answers[dataIndex];
       dataIndex++;
     } else {
@@ -90,15 +77,16 @@ function createGraphData(name, from, to) {
   }
 
   let dataSets = [dataSetsTmp.q1, dataSetsTmp.q2, dataSetsTmp.q3, dataSetsTmp.q4, dataSetsTmp.q5];
-  console.log({labels: labels, datasets: dataSets});
   return {labels: labels, datasets: dataSets};
 }
 
-function getAnswerByName(name) {
-  // TODO 作成
-  // date, name, q1, q2, q3, q4, q5, q6
-  let data = [];
-  data[0] = {
+/**
+ * 名前から回答を取得
+ */
+function getAnswersByName(name) {
+  // TODO: 作成
+  let answers = [];
+  answers[0] = {
       date : new Date(2018, 3, 20),
       q1 : 3,
       q2 : 2,
@@ -106,7 +94,7 @@ function getAnswerByName(name) {
       q4 : 4,
       q5 : 5,
     };
-  data[1] = {
+  answers[1] = {
       date : new Date(2018, 3, 21),
       q1 : 1,
       q2 : 5,
@@ -114,7 +102,7 @@ function getAnswerByName(name) {
       q4 : 4,
       q5 : 4,
     };
-  data[2] = {
+  answers[2] = {
       date : new Date(2018, 3, 23),
       q1 : 2,
       q2 : 5,
@@ -122,9 +110,53 @@ function getAnswerByName(name) {
       q4 : 4,
       q5 : 3,
     };
-  return data;
+  return answers;
 }
 
-function dateToString(date) {
-  return `${date.getMonth() + 1}/${date.getDate()}`;
+/**
+ * 名前の一覧を取得
+ */
+function getAllNames() {
+  // TODO: 作成
+  return ["hoge", "hoge2", "hoge3"];
+}
+
+function getDatasetsTemplate() {
+  return {
+      q1 : {
+          label: "私生活",
+          borderColor: "rgba(249, 71, 66, 1)",
+          backgroundColor: "rgba(249, 71, 66, 0.2)",
+          spanGaps: true,
+          data: [],
+        },
+      q2 : {
+          label: "学校生活",
+          borderColor: "rgba(52, 151, 254, 1)",
+          backgroundColor: "rgba(52, 151, 254, 0.2)",
+          spanGaps: true,
+          data: [],
+        },
+      q3 : {
+          label: "人間関係",
+          borderColor: "rgba(245, 249, 66, 1)",
+          backgroundColor: "rgba(245, 249, 66, 0.2)",
+          spanGaps: true,
+          data: [],
+        },
+      q4 : {
+          label: "体調",
+          borderColor: "rgba(66, 148, 67, 1)",
+          backgroundColor: "rgba(66, 148, 67, 0.2)",
+          spanGaps: true,
+          data: [],
+        },
+      q5 : {
+          label: "プライズ",
+          borderColor: "rgba(249, 176, 66, 1)",
+          backgroundColor: "rgba(249, 176, 66, 0.2)",
+          spanGaps: true,
+          data: [],
+        },
+    };
 }
